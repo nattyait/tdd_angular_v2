4faca07e81a0905acf12e0aaabb7aebdbea85410
Natty <natty@prontomarketing.com>
2013-09-20 15:27:30 +0700

    connect to firebase to get and save data

--- specs/todo_spec.js
+++ specs/todo_spec.js
@@ -6,27 +6,30 @@ describe('TodoController', function() {
 		//controller('name', configuration);
 		$controller('TodoController', {
 			$scope: $scope,
+			TodoService: null,
 		});
 	}));
     it('it should have 2 items after initialize', function() {
     	expect($scope.items.length).toEqual(2);
     });
     it('it should have Learn Angular as first item', function () {
-    	expect($scope.items[0]).toEqual('Learn Angular');
+    	expect($scope.items[0].name).toEqual('Learn Angular');
     });
     it('it should have Build Angular as second item', function(){
-    	expect($scope.items[1]).toEqual('Build Angular')
+    	expect($scope.items[1].name).toEqual('Build Angular')
     });
     describe('add', function () {
     	it('should have 3 items after added', function(){
     		$scope.add('Eat Lunch');
     		expect($scope.items.length).toEqual(3);
+    		expect($scope.items[2].name).toEqual('Eat Lunch');
     	});
     	it('should clear new item box after added', function () {
     		$scope.newItem = "Eat Lunch";
     		$scope.add('Eat Lunch');
     		expect($scope.newItem).toEqual('');
     	});
+    	
     });
 });
 
--- static/js/todo.js
+++ static/js/todo.js
@@ -1,11 +1,40 @@
-function TodoController($scope){
+//inject TodoService to controller
+function TodoController($scope, TodoService){
 	$scope.items = [
-		'Learn Angular',
-		'Build Angular'
+		{name: 'Learn Angular'},
+		{name: 'Build Angular'},
 	];
 
 	$scope.add = function(newItem){
-		$scope.items.push(newItem);
+		$scope.items.push({name: newItem});
 		$scope.newItem = '';
 	}
-}
+
+	$scope.load = function (){
+		$scope.items = TodoService.query();
+	}
+
+	$scope.save = function (){
+		TodoService.save($scope.items);
+	}
+};
+
+//client side
+//below is a function, need to be registered
+todoApp = angular.module('TodoApp', ['ngResource']);
+todoApp.factory('TodoService', TodoService);
+//$scope = first arg to inject
+//$TodoService = secode arg to inject
+//TodoControll'r = last item -- the real controller
+injectList = ['$scope', 'TodoService', 'TodoContoller'];
+todoApp.controller('TodoContoller', injectList);
+function TodoService($resource){
+	var url, defaultParams, actions;
+	url = "https://natty-todo-app.firebaseio.com/todos.json";
+	defaultParams = {};
+	//override because firebase doesn't accept method Post for save action
+	actions = {
+		save: { method: 'PUT', isArray: true}
+	}
+	return $resource(url, defaultParams, actions)
+};
